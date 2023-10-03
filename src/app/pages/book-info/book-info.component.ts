672419de424b1f3ea648a1098cb1abca1326cd4b
99pmore <morenomartin.pablo@gmail.com>
2023-10-03 15:21:32 +0200

    action buttons

--- src/app/pages/book-info/book-info.component.ts
+++ src/app/pages/book-info/book-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from 'src/app/models/book.interface';
+import { BookDB } from 'src/app/models/bookDB.interface';
 import { BookDbService } from 'src/app/services/book-db.service';
 import { BookService } from 'src/app/services/book.service';
 
@@ -30,13 +31,70 @@ export class BookInfoComponent implements OnInit {
   
   read() {
     this.id = this.route.snapshot.paramMap.get('id')!
-    this.bookDBService.addReadBook(this.id)
+
+    this.bookService.getBook(this.id).subscribe((data) => {
+      this.book = data
+    })
+
+    const bookDB: BookDB = {
+      id: this.id,
+      cover: this.book.volumeInfo?.imageLinks?.thumbnail || '',
+      title: this.book.volumeInfo?.title || '',
+      authors: this.book.volumeInfo?.authors || [],
+      categories: this.book.volumeInfo?.categories || [],
+      rating: this.book.volumeInfo?.averageRating || 0,
+      description: this.book.volumeInfo?.description || '',
+      state: 'read',
+    }
+
+    this.bookDBService.addBook(bookDB, 'readBooks')
+  }
+
+  reading() {
+    this.id = this.route.snapshot.paramMap.get('id')!
+
+    this.bookService.getBook(this.id).subscribe((data) => {
+      this.book = data
+    })
+
+    const bookDB: BookDB = {
+      id: this.id,
+      cover: this.book.volumeInfo?.imageLinks?.thumbnail || '',
+      title: this.book.volumeInfo?.title || '',
+      authors: this.book.volumeInfo?.authors || [],
+      categories: this.book.volumeInfo?.categories || [],
+      rating: this.book.volumeInfo?.averageRating || 0,
+      description: this.book.volumeInfo?.description || '',
+      state: 'reading',
+    }
+
+    this.bookDBService.addBook(bookDB, 'readingBooks')
+  }
+
+  wish() {
+    this.id = this.route.snapshot.paramMap.get('id')!
+
+    this.bookService.getBook(this.id).subscribe((data) => {
+      this.book = data
+    })
+
+    const bookDB: BookDB = {
+      id: this.id,
+      cover: this.book.volumeInfo?.imageLinks?.thumbnail || '',
+      title: this.book.volumeInfo?.title || '',
+      authors: this.book.volumeInfo?.authors || [],
+      categories: this.book.volumeInfo?.categories || [],
+      rating: this.book.volumeInfo?.averageRating || 0,
+      description: this.book.volumeInfo?.description || '',
+      state: 'wish',
+    }
+
+    this.bookDBService.addBook(bookDB, 'wishBooks')
   }
 
   private getBook() {
     this.id = this.route.snapshot.paramMap.get('id')!
-
-    this.bookService.getBook(this.id).subscribe((data: any) => {
+    this.bookService.getBook(this.id).subscribe((data) => {
       this.book = data
     })
   }
