4b74566dea93bfd423fb266fc56a6fd5b0047112
99pmore <morenomartin.pablo@gmail.com>
2023-10-03 15:51:06 +0200

    rating stars

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-// import { faStar as faStarSol } from '@fortawesome/free-solid-svg-icons'
-// import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarSol } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-rating',
@@ -11,8 +11,8 @@ export class RatingComponent implements OnInit {
 
   @Input() rating!: number
 
-  // faStarSol = faStarSol
-  // faStarReg = faStarReg
+  faStarSol = faStarSol
+  faStarReg = faStarReg
 
   constructor() { }
 
