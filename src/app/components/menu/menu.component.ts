f00ac2634883793c873ad9a342c37e89a700093d
99pmore <morenomartin.pablo@gmail.com>
2023-10-03 12:22:55 +0200

    userEmail and logout div

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { Auth, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -8,13 +11,25 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class MenuComponent implements OnInit {
 
-  userEmail: string | null = null
+  userEmail!: string | null
+
+  faLogout = faArrowRightFromBracket
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private auth: Auth,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.userEmail = this.authService.getUserEmail()
+  }
+
+  logout() {
+    signOut(this.auth).then(() => {
+      this.router.navigate(['/'])
+    }).catch((error) => {
+      console.log('Error cerrando sesión: ', error)
+    })
   }
 }
