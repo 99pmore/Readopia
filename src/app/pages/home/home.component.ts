7e0a7003e70ad0883c88a73ab7ca7b27e654a2b9
99pmore <morenomartin.pablo@gmail.com>
2023-10-03 15:21:43 +0200

    books lists

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, forkJoin } from 'rxjs';
-import { Book } from 'src/app/models/book.interface';
+import { BookDB } from 'src/app/models/bookDB.interface';
 import { AuthService } from 'src/app/services/auth.service';
 import { BookDbService } from 'src/app/services/book-db.service';
-import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-home',
@@ -15,46 +13,30 @@ export class HomeComponent implements OnInit {
   isUserLoggedIn: boolean = false
   userEmail!: string | null
 
-  readBooksIDs!: string[]
-  readBooks!: Book[]
+  readBooks!: BookDB[]
+  readingBooks!: BookDB[]
+  wishBooks!: BookDB[]
   
   constructor(
     private authService: AuthService,
     private bookDBService: BookDbService,
-    private bookService: BookService
   ) {}
 
   async ngOnInit(): Promise<void> {
     this.isUserLoggedIn = this.authService.isLoggedIn()
     this.userEmail = this.authService.getUserEmail()
 
-    await this.getReadBooksIDs()
-    this.getBooks()
+    await this.getBooks()
   }
 
-  async getReadBooksIDs() {
+  async getBooks() {
     try {
-      this.readBooksIDs = await this.bookDBService.getReadBooks()
+      this.readBooks = await this.bookDBService.getBooks('readBooks')
+      this.readingBooks = await this.bookDBService.getBooks('readingBooks')
+      this.wishBooks = await this.bookDBService.getBooks('wishBooks')
 
     } catch (error) {
       console.error('Error al obtener los libros leídos:', error)
-    }
-  }
-
-  getBooks() {
-    if (this.readBooksIDs) {
-      const requests = this.readBooksIDs.map((bookId) =>
-        this.bookService.getBook(bookId)
-      )
-  
-      forkJoin(requests)
-        .subscribe((booksArray) => {
-          this.readBooks = booksArray
-  
-          console.log(this.readBooks)
-        }, (error) => {
-          console.error('Error al obtener los detalles de los libros:', error)
-        })
     }
   }
 
