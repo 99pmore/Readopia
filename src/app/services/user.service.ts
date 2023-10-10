0187458f3a539cc8b12f4774ce4573974becce2d
99pmore <morenomartin.pablo@gmail.com>
2023-10-10 16:01:38 +0200

    getUser function added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { User } from '@angular/fire/auth';
-import { Firestore, addDoc, collection, doc, setDoc } from '@angular/fire/firestore';
+import { Auth, User } from '@angular/fire/auth';
+import { Firestore, addDoc, collection, doc, getDoc, setDoc } from '@angular/fire/firestore';
+import { UserDB } from '../models/userDB.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,45 @@ import { Firestore, addDoc, collection, doc, setDoc } from '@angular/fire/firest
 export class UserService {
 
   constructor(
-    private firestore: Firestore
+    private firestore: Firestore,
+    private auth: Auth
   ) { }
 
   addUser(user: User) {
     const userRef = doc(this.firestore, 'users', user.uid)
     return setDoc(userRef, {
+      name: user.displayName?.split(' ')[0],
+      lastname: user.displayName?.split(' ')[1].concat(' ', user.displayName.split(' ')[2]),
       email: user.email,
       readBooks: [],
       readingBooks: [],
-      wishBooks: []
+      wishBooks: [],
+      favorites: []
     })
+  }
+
+  async getUser(): Promise<UserDB> {
+    try {
+      const user = this.auth.currentUser
+
+      if (user) {
+        const userRef = doc(this.firestore, `users/${user.uid}`)
+        const userDoc = getDoc(userRef)
+
+        if ((await userDoc).exists()) {
+          const userData = (await userDoc).data()
+
+          if (userData && userData) {
+            return userData
+          }
+        }
+      }
+
+      return {}
+
+    } catch (error) {
+      console.log('Error al obtener los usuarios: ', error)
+      throw(error)
+    }
   }
 }
