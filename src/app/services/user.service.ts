cdce99def5cf80541dbb0600aac9e2426a08df46
99pmore <morenomartin.pablo@gmail.com>
2023-10-25 17:10:17 +0200

    added getUsers method and following functionality

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Auth, User } from '@angular/fire/auth';
-import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
+import { Firestore, collection, doc, getDoc, getDocs, setDoc, arrayUnion, updateDoc, arrayRemove } from '@angular/fire/firestore';
 import { UserDB } from '../models/userDB.interface';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -32,6 +33,27 @@ export class UserService {
     })
   }
 
+  async getUsers(): Promise<UserDB[]> {
+    try {
+      const usersRef = collection(this.firestore, 'users')
+      const querySnapshot = await getDocs(usersRef)
+
+      const users: UserDB[] = []
+      querySnapshot.forEach((doc) => {
+        if (doc.exists()) {
+          const userData = doc.data()
+          users.push(userData)
+        }
+      })
+
+      return users
+
+    } catch (error) {
+      console.log('Error al obtener los usuarios: ', error)
+      throw error
+    }
+  }
+
   async getUserById(userId: string): Promise<UserDB> {
     try {
       if (userId) {
@@ -50,5 +72,96 @@ export class UserService {
       console.log('Error al obtener los usuarios: ', error)
       throw(error)
     }
+  }
+
+  async addFollow(followId: string) {
+    const matchingUser = await this.isFollowing(followId)
+
+    if (!matchingUser) {
+      const userId = this.auth.currentUser?.uid
+      if (userId) {
+        const userRef = doc(this.firestore, `users/${userId}`)
+        await updateDoc(userRef, {
+          following: arrayUnion(followId)
+        })
+
+        const userRefFollow = doc(this.firestore, `users/${followId}`)
+        await updateDoc(userRefFollow, {
+          followers: arrayUnion(userId)
+        })
+
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `Usuario seguido`,
+          showConfirmButton: false,
+          timer: 1500
+        })
+  
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'El usuario no está autenticado',
+          footer: '<a href="/login">Iniciar sesión</a>'
+        })
+      }
+
+    } else {
+
+    }
+  }
+
+  async isFollowing(followId: string): Promise<boolean> {
+    const userId = this.auth.currentUser?.uid
+    if (userId) {
+      const user = await this.getUserById(userId)
+      return user.following?.includes(followId) || false
+    }
+
+    return false
+  }
+
+  async deleteFollowing(followId: string) {
+    Swal.fire({
+      title: '¿Estás seguro/a?',
+      text: `Dejarás de seguir a este usuario`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#9e9682',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar'
+      
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const userId = this.auth.currentUser?.uid
+        if (userId) {
+          const userRef = doc(this.firestore, `users/${userId}`)
+          await updateDoc(userRef, {
+            following: arrayRemove(followId)
+          })
+
+          const userRefFollow = doc(this.firestore, `users/${followId}`)
+          await updateDoc(userRefFollow, {
+            followers: arrayRemove(userId)
+          })
+    
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'El usuario no está autenticado',
+            footer: '<a href="/login">Iniciar sesión</a>'
+          })
+        }
+    
+        Swal.fire(
+          '',
+          'Has dejado de seguir a este usuario',
+          'success'
+        )
+      }
+    })
   }
 }
