d9b3bd048692832b2fe8a5fb3b85f4d7fb2c0732
99pmore <morenomartin.pablo@gmail.com>
2023-10-27 15:41:00 +0200

    added character photos selection

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,26 +1,35 @@
+import { ProfilePhotosService } from './../../services/profile-photos.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { AuthService } from 'src/app/services/auth.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss'],
     standalone: true,
-    imports: [MenuComponent, FormsModule, ReactiveFormsModule, RouterLink]
+    imports: [MenuComponent, FormsModule, ReactiveFormsModule, RouterLink, NgFor, NgIf]
 })
 export class RegisterComponent implements OnInit {
+
+  profilePhotos!: any[]
+  selectedPhoto!: string
+  photoSelectionOpen: boolean = false
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
+    private profilePhotosService: ProfilePhotosService
   ) { }
 
   registerForm!: FormGroup
 
   ngOnInit(): void {
+    this.getProfilePhotos()
+
     this.registerForm = this.fb.group({
       name: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
@@ -29,12 +38,29 @@ export class RegisterComponent implements OnInit {
     })
   }
 
+  openPhotoSelection() {
+    this.photoSelectionOpen = !this.photoSelectionOpen
+  }
+  
+  selectProfilePhoto(image: string) {
+    this.selectedPhoto = image
+    this.photoSelectionOpen = false
+  }
+
   register() {
+    const photo = this.selectedPhoto
+
     const name = this.registerForm.get('name')?.value
     const lastname = this.registerForm.get('lastname')?.value
     const email = this.registerForm.get('email')?.value
     const password = this.registerForm.get('password')?.value
 
-    this.authService.register(name, lastname, email, password)
+    this.authService.register(photo, name, lastname, email, password)
+  }
+
+  private getProfilePhotos() {
+    this.profilePhotosService.getPhotos().subscribe(images => {
+      this.profilePhotos = images
+    })
   }
 }
