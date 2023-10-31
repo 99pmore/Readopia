021ea52716cd79fbe02981120e9bd6458e614c26
99pmore <morenomartin.pablo@gmail.com>
2023-10-31 11:10:40 +0100

    ngOptimizedImage added

--- src/app/components/sm-cover/sm-cover.component.ts
+++ src/app/components/sm-cover/sm-cover.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { BookDB } from 'src/app/models/bookDB.interface';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-sm-cover',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgOptimizedImage],
   templateUrl: './sm-cover.component.html',
   styleUrls: ['./sm-cover.component.scss']
 })
