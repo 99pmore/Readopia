22ef1bb4cd8e2ee5e04176f21222a7a0cfc2ec44
99pmore <morenomartin.pablo@gmail.com>
2023-11-06 16:43:43 +0100

    reactive reviews

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -2,43 +2,78 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RatingComponent } from '../rating/rating.component';
 import { ReviewsService } from 'src/app/services/reviews.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Review } from 'src/app/models/review.interface';
 import { UserService } from 'src/app/services/user.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [CommonModule, RatingComponent],
+  imports: [CommonModule, RatingComponent, RouterLink],
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent implements OnInit {
   
+  bookId!: string
+
   reviews: Review[] = []
   name!: string | undefined
   lastName!: string | undefined
   userNames: { [key: string]: string } = {}
   userPhotos: { [key: string]: string } = {}
 
+  authId!: string | undefined
+
+  private reviewsSubscription!: Subscription
+
   constructor (
     private route: ActivatedRoute,
     private reviewsService: ReviewsService,
-    private userService: UserService
+    private userService: UserService,
+    private authService: AuthService
   ) {}
 
   async ngOnInit(): Promise<void> {
-    const bookId = this.route.snapshot.paramMap.get('id')!
-    if (bookId !== undefined) {
-      this.reviews = await this.reviewsService.getReviews(bookId)
-      this.getUser()
+    this.authService.authChanges().subscribe((user) => {
+      this.authId = user?.uid
+    })
+
+    this.bookId = this.route.snapshot.paramMap.get('id')!
+
+    if (this.bookId !== undefined) {
+      this.getReviews()
+
+      this.reviewsSubscription = this.reviewsService.updatedReviews$
+      .subscribe(async () => {
+        this.getReviews()
+      })
+
     }
   }
 
-  async getUser() {
+  ngOnDestroy(): void {
+    if (this.reviewsSubscription) {
+      this.reviewsSubscription.unsubscribe()
+    }
+  }
+
+  public deleteReview(reviewId: string | undefined) {
+    if (reviewId) this.reviewsService.deleteReview(reviewId)
+  }
+
+  private async getReviews() {
+    this.reviews = await this.reviewsService.getReviews(this.bookId)
+    this.getUser()
+  }
+
+  private async getUser() {
     this.reviews.map(async (review) => {
       if (review.userId) {
         const user = await this.userService.getUserById(review.userId)
+
         this.userNames[review.userId] = `${user.name} ${user.lastname}`
         if (user.photo) this.userPhotos[review.userId] = user.photo
       }
