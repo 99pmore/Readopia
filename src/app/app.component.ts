ea048938786ac01e29a744cc44dd9682894c9421
99pmore <morenomartin.pablo@gmail.com>
2023-11-06 16:56:33 +0100

    unified header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, MenuComponent]
 })
 export class AppComponent {
   title = 'Readopia';
