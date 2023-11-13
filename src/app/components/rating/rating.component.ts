8ff4229767e29421eb1129df17b3baf329047c53
99pmore <morenomartin.pablo@gmail.com>
2023-11-13 11:43:59 +0100

    added half stars for decimal ratings

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { faStar as faStarSol } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarSol, faStarHalfAlt as faStarHalf } from '@fortawesome/free-solid-svg-icons'
 import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgFor } from '@angular/common';
@@ -15,12 +15,28 @@ export class RatingComponent implements OnInit {
 
   @Input() rating!: number | undefined
 
-  faStarSol = faStarSol
-  faStarReg = faStarReg
+  public faStarSol = faStarSol
+  public faStarReg = faStarReg
+  public faStarHalf = faStarHalf
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public showHalfStar(index: number): boolean {
+    if (this.rating === undefined) {
+      return false
+    }
+  
+    return this.rating > index && this.rating < index + 1
+  }
+
+  public floorRating(): number {
+    if (this.rating === undefined) {
+      return 0
+    }
+  
+    return Math.floor(this.rating)
+  }
 }
