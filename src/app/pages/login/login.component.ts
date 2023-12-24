3633afed5f951f5c3b4fb266613e01fc5b9bfc2d
99pmore <morenomartin.pablo@gmail.com>
2023-12-24 15:08:44 +0100

    form errors managing

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,13 +3,15 @@ import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } 
 import { RouterLink } from '@angular/router';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { AuthService } from 'src/app/services/auth.service';
+import { NgIf } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
     standalone: true,
-    imports: [MenuComponent, FormsModule, ReactiveFormsModule, RouterLink]
+    imports: [MenuComponent, FormsModule, ReactiveFormsModule, RouterLink, NgIf]
 })
 export class LoginComponent implements OnInit {
 
@@ -26,14 +28,47 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required]],
     })
   }
 
-  login() {
-    const email = this.loginForm.get('email')?.value
-    const password = this.loginForm.get('password')?.value
+  public login() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value
+      const password = this.loginForm.get('password')?.value
+  
+      this.authService.login(email, password)
 
-    this.authService.login(email, password)
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'El formulario no es válido',
+      })
+    }
+  }
+
+  public validLogin(info: string) {
+    return this.loginForm.get(info)?.invalid && (this.loginForm.get(info)?.dirty || this.loginForm.get(info)?.touched)
+  }
+
+  public errorMessage(info: string) {
+    let error: string = ''
+
+    if (info === 'email') {
+      if (this.loginForm.get(info)?.errors?.['required']) {
+        error = 'Introduce tu correo electrónico'
+      }
+      else if (this.loginForm.get(info)?.errors?.['email']) {
+        error = 'El formato no es válido'
+      }
+    
+    } else if (info === 'password') {
+      if (this.loginForm.get(info)?.errors?.['required']) {
+        error = 'Introduce una contraseña'
+      }
+    }
+
+    return error
   }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,6 +5,7 @@ import { MenuComponent } from '../../components/menu/menu.component';
 import { AuthService } from 'src/app/services/auth.service';
 import { NgFor, NgIf } from '@angular/common';
 import { CharacterPhotoSelectorComponent } from 'src/app/components/character-photo-selector/character-photo-selector.component';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-register',
@@ -37,14 +38,60 @@ export class RegisterComponent implements OnInit {
     this.selectedPhoto = photo
   }
 
-  register() {
-    const photo = this.selectedPhoto
+  public register() {
+    if (this.registerForm.valid) {
+      const photo = this.selectedPhoto
 
-    const name = this.registerForm.get('name')?.value
-    const lastname = this.registerForm.get('lastname')?.value
-    const email = this.registerForm.get('email')?.value
-    const password = this.registerForm.get('password')?.value
+      const name = this.registerForm.get('name')?.value
+      const lastname = this.registerForm.get('lastname')?.value
+      const email = this.registerForm.get('email')?.value
+      const password = this.registerForm.get('password')?.value
+  
+      this.authService.register(photo, name, lastname, email, password)
 
-    this.authService.register(photo, name, lastname, email, password)
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'El formulario no es válido',
+      })
+    }
+  }
+
+  public validRegister(info: string) {
+    return this.registerForm.get(info)?.invalid && (this.registerForm.get(info)?.dirty || this.registerForm.get(info)?.touched)
+  }
+
+  public errorMessage(info: string) {
+    let error: string = ''
+
+    if (info === 'name') {
+      if (this.registerForm.get(info)?.errors?.['required']) {
+        error = 'Introduce tu nombre'
+      }
+
+    } else if (info === 'lastname') {
+      if (this.registerForm.get(info)?.errors?.['required']) {
+        error = 'Introduce tus apellidos'
+      }
+
+    } else if (info === 'email') {
+      if (this.registerForm.get(info)?.errors?.['required']) {
+        error = 'Introduce tu correo electrónico'
+      }
+      else if (this.registerForm.get(info)?.errors?.['email']) {
+        error = 'El formato no es válido'
+      }
+    
+    } else if (info === 'password') {
+      if (this.registerForm.get(info)?.errors?.['required']) {
+        error = 'Introduce una contraseña'
+      }
+      else if (this.registerForm.get(info)?.errors?.['minlength']) {
+        error = 'La contraseña debe tener al menos 8 caracteres'
+      }
+    }
+
+    return error
   }
 }
