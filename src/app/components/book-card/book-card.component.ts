1d79c38aba391c9f3651833609004d5db842df55
99pmore <morenomartin.pablo@gmail.com>
2024-02-20 20:35:35 +0100

    unified db-book-card and book-card

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Book } from 'src/app/models/book.interface';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { BookDB } from 'src/app/models/bookDB.interface';
 
 @Component({
     selector: 'app-book-card',
     templateUrl: './book-card.component.html',
     styleUrls: ['./book-card.component.scss'],
     standalone: true,
-    imports: [RouterLink, NgFor]
+    imports: [RouterLink, NgFor, NgIf]
 })
 export class BookCardComponent {
-  @Input() book!: Book
+
+  @Input() book!: Book | BookDB
+
+  public isBookDB(book: any): book is BookDB {
+    return (book as BookDB).cover !== undefined
+  }
 }
