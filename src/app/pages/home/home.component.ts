0e702a4028a1cda2fa48058c42a65733e24dd569
99pmore <morenomartin.pablo@gmail.com>
2024-02-20 20:35:56 +0100

    changed db-book-card to book-card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { User } from '@angular/fire/auth';
 import { BookDB } from 'src/app/models/bookDB.interface';
 import { AuthService } from 'src/app/services/auth.service';
 import { BookDbService } from 'src/app/services/book-db.service';
-import { DbBookCardComponent } from '../../components/db-book-card/db-book-card.component';
+import { BookCardComponent } from 'src/app/components/book-card/book-card.component';
 import { NgIf, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
@@ -14,7 +14,7 @@ import { LoginLinkComponent } from 'src/app/components/login-link/login-link.com
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: true,
-    imports: [HeaderComponent, RouterLink, NgIf, NgFor, DbBookCardComponent, LoginLinkComponent]
+    imports: [HeaderComponent, RouterLink, NgIf, NgFor, BookCardComponent, LoginLinkComponent]
 })
 export class HomeComponent implements OnInit {
 
