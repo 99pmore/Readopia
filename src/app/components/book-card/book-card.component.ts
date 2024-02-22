9a0c9abe522b80c91e4b7e54c6bad2aa3bab5ea1
99pmore <morenomartin.pablo@gmail.com>
2024-02-22 22:49:41 +0100

    show max 3 authors

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,3 +1,4 @@
+import { VolumeInfo } from './../../models/book.interface';
 import { Component, Input } from '@angular/core';
 import { Book } from 'src/app/models/book.interface';
 import { NgFor, NgIf } from '@angular/common';
@@ -17,5 +18,9 @@ export class BookCardComponent {
 
   public isBookDB(book: any): book is BookDB {
     return (book as BookDB).cover !== undefined
+  }
+
+  public getDisplayedAuthors(book: Book): string[] {
+    return book.volumeInfo?.authors?.slice(0, 3) || []
   }
 }
