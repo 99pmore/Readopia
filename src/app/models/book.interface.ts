ae93fba4635ee6113f1dd386660a59ea655a69f9
99pmore <morenomartin.pablo@gmail.com>
2024-03-11 13:38:35 +0100

    deleted non used attributes

--- src/app/models/book.interface.ts
+++ src/app/models/book.interface.ts
@@ -1,54 +1,24 @@
 export interface Data {
-    totalItems?: number;
     items?:      Book[];
 }
 
 export interface Book {
     id?:         string;
-    etag?:       string;
-    selfLink?:   string;
     volumeInfo?: VolumeInfo;
-    searchInfo?: SearchInfo;
-}
-
-export interface SearchInfo {
-    textSnippet?: string;
 }
 
 export interface VolumeInfo {
     title?:               string;
-    industryIdentifiers?: IndustryIdentifier[];
     pageCount?:           number;
     categories?:          string[];
-    language?:            Language;
-    previewLink?:         string;
-    infoLink?:            string;
-    canonicalVolumeLink?: string;
     authors?:             string[];
     publishedDate?:       string;
     averageRating?:       number;
     ratingsCount?:        number;
-    publisher?:           string;
     description?:         string;
     imageLinks?:          ImageLinks;
-    subtitle?:            string;
 }
 
 export interface ImageLinks {
-    smallThumbnail?: string;
     thumbnail?:      string;
-}
-
-export interface IndustryIdentifier {
-    type?:       Type;
-    identifier?: string;
-}
-
-export enum Type {
-    Isbn10 = "ISBN_10",
-    Isbn13 = "ISBN_13",
-}
-
-export enum Language {
-    Es = "es",
 }
